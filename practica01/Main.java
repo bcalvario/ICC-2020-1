8784e3d6a3f73b4b7bd99c5113081d44677f95fb
bcalvario <bcalvario@ciencias.unam.mx>
2019-08-18 16:37:03 -0500

    practica01

---
+++ practica01/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+	public static void main(String[] args){
+		System.out.println("Hola");
+		System.out.println("java 11.0.2 2019-01-15 LTS");
+		System.out.println("Java(TM) SE Runtime Environment 18.9 (build 11.0.2+9-LTS)");
+		System.out.println("Java HotSpot(TM) 64-Bit Server VM 18.9 (build 11.0.2+9-LTS, mixed mode)");
+		System.out.println("");
+		System.out.println("Linux calvario-Lenovo-YOGA-710-14IKB 4.15.0-55-generic #60-Ubuntu SMP Tue Jul 2 18:22:20 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux");
+
+	}
+}
