86a1f3b67769d8eba1aefb9f6d96d56ec017737d
bcalvario <bcalvario@ciencias.unam.mx>
2019-09-19 20:41:23 -0500

    practica08.2

---
+++ practica08-2/ej-svg/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"geometria","l":"Circulo"},{"p":"geometria","l":"Linea"},{"p":"geometria","l":"Punto"},{"p":"svg","l":"SVG"},{"p":"geometria","l":"Triangulo"}]
